8e2a2019df394a4db526b8cd209fef44587cd502
ibrainiac <franqhazard@gmail.com>
2024-06-13 00:16:00 +0300

    rustlings solutions and quizes

---
+++ src/3_if/if3.rs
@@ -0,0 +1,65 @@
+// if3.rs
+//
+// Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
+
+// I AM NOT DONE
+
+pub fn animal_habitat(animal: &str) -> &'static str {
+    let identifier = if animal == "crab" {
+        1
+    } else if animal == "gopher" {
+        2
+    } else if animal == "snake" {
+        3
+    } else {
+        0
+    };
+
+    // DO NOT CHANGE THIS STATEMENT BELOW
+    let habitat = if identifier == 1 {
+        "Beach"
+    } else if identifier == 2 {
+        "Burrow"
+    } else if identifier == 3 {
+        "Desert"
+    } else {
+        "Unknown"
+    };
+
+    habitat
+}
+
+// No test changes needed.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gopher_lives_in_burrow() {
+        assert_eq!(animal_habitat("gopher"), "Burrow")
+    }
+
+    #[test]
+    fn snake_lives_in_desert() {
+        assert_eq!(animal_habitat("snake"), "Desert")
+    }
+
+    #[test]
+    fn crab_lives_on_beach() {
+        assert_eq!(animal_habitat("crab"), "Beach")
+    }
+
+    #[test]
+    fn unknown_animal() {
+        assert_eq!(animal_habitat("dinosaur"), "Unknown")
+    }
+}
+// in this exercise, I learned about:
+
+// Consistent Types: Ensuring all branches of a conditional return the same type.
+// Using Identifiers: Using integer identifiers to map to different values.
+// Conditional Logic: Implementing multiple conditions using if-else chains
+// Key Takeaways:
+
+// Consistency: Using consistent types in conditional expressions.
+// Conditional Mapping: Mapping identifiers to specific values using conditional logic.
